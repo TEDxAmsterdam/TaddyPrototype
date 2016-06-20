374546f448d024b269e7f7231a5b19e2b06a922b
Randy Daal <randy@randynamic.com>
2016-06-21 00:11:49 +0200

    simplified the stuff

--- src/components/Messenger/Messenger.js
+++ src/components/Messenger/Messenger.js
@@ -1,96 +1,94 @@
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './Messenger.css';
+import MessengerStyles from './Messenger.css';
 const cx = require('classnames');
 import { connect } from 'react-redux';
 import { sendMsg, updateMsger } from '../../redux/actions/messenger';
 import fetch from 'isomorphic-fetch';
 
-class Messenger extends Component {
+@withStyles(MessengerStyles)
+@connect(state => ({ messenger: state.messenger }), { sendMsg, updateMsger})
+export default class Messenger extends Component {
   static propTypes = {
     messenger: PropTypes.object,
     sendMsg: PropTypes.func.isRequired,
     updateMsger: PropTypes.func.isRequired,
   };
 
-	chatWithBot(props) {
-				fetch(`/api/bot?message=${props.messenger.input}`)
-				.then(response => response.json())
-				.then(function(data) {
-					if(data.text) {
-						console.log(data, 'from chatbot');
-						let update = {
-							user: {
-								avatar: '//pi.tedcdn.com/r/pe.tedcdn.com/images/ted/c9928d59974a7d5b8f8889794634cbded07ff266_1600x1200.jpg?c=1050%2C550&w=180',
-							className: s.them
-							},
-							text: data.text,
-							time: new Date().getTime()
-						};
-						props.sendMsg(update);
-					}
-				});
-			return props.messenger.input;
-	};
+  chatWithBot() {
+    const { messenger, sendMsg } = this.props;
 
-	componentDidMount() {
+    fetch(`/api/bot?message=${messenger.input}`)
+      .then(response => response.json())
+      .then((data) => {
+        if (data.text) {
+          const update = {
+            user: {
+              avatar: '//pi.tedcdn.com/r/pe.tedcdn.com/images/ted/c9928d59974a7d5b8f8889794634cbded07ff266_1600x1200.jpg?c=1050%2C550&w=180',
+              className: MessengerStyles.them,
+            },
+            text: data.text,
+            time: new Date().getTime(),
+          };
+          sendMsg(update);
+        }
+      });
 
-	};
+    return messenger.input;
+  }
 
-	render() {
-    return (<div className={s.wrapper}>
-		     <nav id="nav" className={s.nav}>
-					 <div className={s.defaultNav}>
-			        <div className={s.mainNav}>
-			          <div className={s.toggle}>
-			          </div>
-			          <div className={s.mainNavItem}>
-			            <a className={s.mainNavItemLink}>Chat</a>
-			          </div>
-			          <div className={s.options}>
-			          </div>
-			        </div>
-					</div>
-		     </nav>
-		      <div className={s.inner}>
-		        <div className={s.content}>
-							{this.props.messenger.messageList.map((item, index) => (
-								<div key={index} className={cx(s.messageWrapper, item.user.className)}>
-			            <div className={cx(s.circleWrapper, "animated bounceIn")} style={{backgroundImage:`url("${item.user.avatar}")`, backgroundPosition:'-70px -285px'}}></div>
-			            <div className={s.textWrapper}>{item.text}</div>
-			          </div>
-							))}
-						</div>
-		      </div>
-		      <div className={s.bottom}>
-		          <textarea className={s.input} onChange={(e) => {
-                  this.props.updateMsger(e.target.value);
-                  e.preventDefault();
-                }}></textarea>
-		          <div className={s.send} onClick={(e) => {
-                  this.props.sendMsg({
-						        user: {
-											avatar:'',
-											className: s.me
-										},
-						        text: this.chatWithBot(this.props),
-						        time: new Date().getTime()
-						      });
-                  e.preventDefault();
-                }}></div>
-		      </div>
-	     </div>
-		);
+  onTextInput(e) {
+    this.props.updateMsger(e.target.value);
+  }
+
+  onSendButtonClicked() {
+    this.props.sendMsg({
+      user: {
+        avatar: '',
+        className: MessengerStyles.me,
+      },
+      text: this.chatWithBot(),
+      time: new Date().getTime(),
+    });
+  }
+
+  render() {
+    const messages = this.props.messenger.messageList.map((item, index) => (
+      <div key={index} className={cx(MessengerStyles.messageWrapper, item.user.className)}>
+        <div className={cx(MessengerStyles.circleWrapper, "animated bounceIn")}
+             style={{backgroundImage:`url("${item.user.avatar}")`,
+             backgroundPosition:'-70px -285px'
+             }}>
+        </div>
+        <div className={MessengerStyles.textWrapper}>{item.text}</div>
+      </div>
+    ));
+
+    return (
+      <div className={MessengerStyles.wrapper}>
+        <nav id="nav" className={MessengerStyles.nav}>
+          <div className={MessengerStyles.defaultNav}>
+            <div className={MessengerStyles.mainNav}>
+              <div className={MessengerStyles.toggle}>
+              </div>
+              <div className={MessengerStyles.mainNavItem}>
+                <a className={MessengerStyles.mainNavItemLink}>Chat</a>
+              </div>
+              <div className={MessengerStyles.options}>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div className={MessengerStyles.inner}>
+          <div className={MessengerStyles.content}>
+            {messages}
+          </div>
+        </div>
+        <div className={MessengerStyles.bottom}>
+          <textarea className={MessengerStyles.input} onChange={::this.onTextInput}></textarea>
+          <div className={MessengerStyles.send} onClick={::this.onSendButtonClicked}></div>
+        </div>
+      </div>
+    );
   }
 }
-
-const mapState = state => ({
-  messenger: state.messenger,
-});
-
-const mapDispatch = {
-  sendMsg,
-  updateMsger,
-};
-
-export default connect(mapState, mapDispatch)(withStyles(s)(Messenger));
