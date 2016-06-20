da424fa237b4f24948cae2f5fe0a2636493f287e
Randy Daal <randy@randynamic.com>
2016-06-20 22:35:23 +0200

    Object literal is a neater solution for this

--- src/redux/reducers/messenger.js
+++ src/redux/reducers/messenger.js
@@ -1,23 +1,20 @@
-import * as ActionTypes from '../constants';
+import { MESSENGER_UPDATE, MESSENGER_SEND } from '../constants';
 
-let defaultState = {
+const defaultState = {
   messageList: [],
   input: '',
 };
 
+const actionsMap = {
+  [MESSENGER_UPDATE]: (state, action) => ({ ...state, input: action.payload.message }),
+  [MESSENGER_SEND]: (state, action) => (
+    { ...state, messageList: [...state.messageList, action.payload.message] }
+  ),
+};
+
 export default function messenger(state = defaultState, action) {
-  switch (action.type) {
-    case ActionTypes.MESSENGER_UPDATE:
-      return {
-        ...state,
-        input: action.payload.message,
-      };
-    case ActionTypes.MESSENGER_SEND:
-		  state.messageList.push(action.payload.message);
-      return {
-        ...state,
-      };
-    default:
-      return state;
-  }
+  const reduceFn = actionsMap[action.type];
+  if (!reduceFn) return state;
+
+  return { ...state, ...reduceFn(state, action) };
 }
